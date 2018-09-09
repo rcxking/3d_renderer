e00d32e49cb7c4505e894491f6d00044c956b424
Bryant Pong <rcxking@gmail.com>
2018-09-09 16:22:49 -0700

    Adding Tuple Operators
    This commit adds tuple operators for addition, subtraction, and
    negation.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

--- src/Tuple.h
+++ src/Tuple.h
@@ -11,6 +11,8 @@
 #ifndef _TUPLE_H_
 #define _TUPLE_H_
 
+#include <cmath>
+
 class Tuple
 {
 public:
@@ -30,6 +32,76 @@ public:
         m_z( z ),
         m_isPoint( isPoint ) {}
 
+    // Copy Constructor
+    Tuple( const Tuple& rhs ) :
+        m_x( rhs.m_x ),
+        m_y( rhs.m_y ),
+        m_z( rhs.m_z ),
+        m_isPoint( rhs.m_isPoint ) {}
+
+    // Destructor
+    ~Tuple() {}
+
+    // Assignment operator=
+    Tuple& operator=( const Tuple& rhs )
+    {
+        // Check for self-referencing:
+        if( this != &rhs )
+        {
+            m_x = rhs.m_x;
+            m_y = rhs.m_y;
+            m_z = rhs.m_z;
+            m_isPoint = rhs.m_isPoint;
+        }
+        return *this;
+    }
+
+    // Assignment operator==
+    bool operator==( const Tuple& rhs ) const
+    {
+       return IsEqual( m_x, rhs.m_x ) &&
+              IsEqual( m_y, rhs.m_y ) &&
+              IsEqual( m_z, rhs.m_z ) &&
+              ( m_isPoint == rhs.m_isPoint );
+    }
+
+    // operator+= 
+    Tuple& operator+=( const Tuple& rhs )
+    {
+        m_x += rhs.m_x;
+        m_y += rhs.m_y;
+        m_z += rhs.m_z;
+        m_isPoint = m_isPoint || rhs.m_isPoint; 
+        return *this;
+    }
+
+    // operator-=
+    Tuple& operator-=( const Tuple& rhs )
+    {
+        m_x -= rhs.m_x;
+        m_y -= rhs.m_y;
+        m_z -= rhs.m_z;
+        m_isPoint -= rhs.m_isPoint;
+    }
+
+    // operator+
+    const Tuple operator+( const Tuple& rhs )
+    {
+        return Tuple( *this ) += rhs;
+    }
+
+    // operator-
+    const Tuple operator-( const Tuple& rhs )
+    {
+        return Tuple( *this ) -= rhs;
+    }
+
+    // Negation operator:
+    const Tuple operator-()
+    {
+        return Tuple( -1 * m_x, -1 * m_y, -1 * m_z, m_isPoint ); 
+    }
+
     // Accessors/Modifiers:
     inline double GetX() const { return m_x; }
     inline double GetY() const { return m_y; }
@@ -43,11 +115,19 @@ public:
     void SetIsPoint( const bool isPoint ) { m_isPoint = isPoint; }
 
 private:
-   // (x, y, z) coordinates:
-   double m_x, m_y, m_z;
 
-   // Does this tuple represent a point?
-   bool m_isPoint;
+    // Comparing floating point numbers with each other safely:
+    bool IsEqual( const double lhs, const double rhs ) const
+    {
+        const double EPSILON = 0.0001;
+        return fabs( lhs - rhs ) < EPSILON;
+    }
+
+    // (x, y, z) coordinates:
+    double m_x, m_y, m_z;
+
+    // Does this tuple represent a point?
+    bool m_isPoint;
 };
 
 // Derived classes to aid in the creation of Points/Vectors
