a7fcd7f1007b0b80f0d42cdc30fbe3c675965624
Bryant Pong <rcxking@gmail.com>
2019-12-29 22:36:01 -0800

    Adding initial world_tests.h

---
+++ tests/world_tests.h
@@ -0,0 +1,24 @@
+#ifndef __WORLD_TESTS_H_
+#define __WORLD_TESTS_H_
+/*
+ * world_tests.h
+ *
+ * Unit tests for a World object that keeps track of objects and light sources
+ * within a scene.
+ *
+ * Bryant Pong
+ * 12/29/19
+ */
+#include "World.h"
+
+SCENARIO("a world is created", "[World]") {
+  GIVEN("a default world is created") {
+    World world;
+
+    THEN("it is created with no objects and no light source") {
+      REQUIRE(world.GetObjects().size() == 0):
+      REQUIRE(world.HasLightSource()    == false);
+    }
+  }
+} 
+#endif
